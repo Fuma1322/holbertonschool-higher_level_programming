7c1cfd61153c2c1a61648ba6fc3c39d6046f04e0
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-01-25 18:03:55 +0200

    Python

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{:02d}".format(i), end=", ")
 
-if i <= 0 <= 99:
-    print("{:02d}".formart(i))
+print(", ".join("{:02d}".format(i) for i in range(98)))
