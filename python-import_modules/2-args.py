998b5f8e0315019b94f7405635d9080c48428be2
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-01-27 14:20:19 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
+
+if len(argv) == 1:
+    print("0 arguments.")
+else:
+    print("{:d} argument{}:".format(len(argv) - 1, 's' if len(argv) > 2 else ''))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
