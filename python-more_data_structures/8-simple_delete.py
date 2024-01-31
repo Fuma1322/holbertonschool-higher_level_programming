9c060ae62c6a3f362a3286f317dba33151a2ae44
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-01-31 18:10:22 +0200

    Update 8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
 #!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
