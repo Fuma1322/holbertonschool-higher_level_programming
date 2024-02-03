a916a5e1634e4f3dfafa9200e568f9225a250b05
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-03 17:52:51 +0200

    Python

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-class Square:
-    """Class Square that defines a square object
-    """
-    def __init__(self, size):
-        """Initialize method that stores the size of the square
+# -*- coding: UTF-8 -*-
+"""
+1-square.py: is a class Square that defines a square
+"""
 
-        Args:
-            param1 (int): size of the square
-        """
+
+class Square:
+    """class Square that defines a square
+
+    Attributes:
+        attr1 (size): Size of the Square.
+
+    """
+
+    def __init__(self, size=0):
+        """Initializer _Square__size"""
         self.__size = size
