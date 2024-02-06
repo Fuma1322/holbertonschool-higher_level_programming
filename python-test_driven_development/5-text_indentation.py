b65532b2fe8c75a2605b5890da30e5b659884f24
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-06 15:19:55 +0200

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""Text indentation module"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each characters: ., ? and :
+    """Print text with two new lines after each '.', '?', and ':'.
+
     Args:
-        text (str): The text to print.
+        text (string): The text to print.
     Raises:
         TypeError: If text is not a string.
-    Returns:
-        None: The text is printed.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace('.', '.\n\n')
-    text = text.replace('?', '?\n\n')
-    text = text.replace(':', ':\n\n')
-    print("\n".join([line.strip() for line in text.split("\n")]), end="")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
