bbb9de0edc333aaf7f3600c97ae88b30f81aa451
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-10 15:11:52 -0500

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,24 +1,12 @@
+02 
+
 #!/usr/bin/python3
-
-"""
-This module provides a function to look up
-attributes and methods of an object.
-"""
+"""Defines an inherited list class MyList."""
 
 
-def lookup(obj):
-    """
-    Returns a list of names in the given
-    object's namespace, including attributes
-    and methods.
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
-    Args:
-        obj (object): The object whose attributes
-        and methods should be listed.
-
-    Returns:
-        list: A list of strings representing
-        the names of the object's attributes
-              and methods.
-    """
-    return dir(obj)
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
