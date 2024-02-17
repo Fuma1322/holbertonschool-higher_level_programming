8445826d3e56ae06c9e4130c315e80f5036c8222
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-17 12:23:14 +0200

    Update square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,3 +33,34 @@ class Square(Rectangle):
     def size(self):
         """Getter for width and height properties of the rectangle."""
         return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for width and height properties of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates the position or dimensions of the square."""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Convert the Square object into a dictionary."""
+        return {
+            "x": self.x,
+            "y": self.y,
+            "id": self.id,
+            "size": self.size,
+        }
